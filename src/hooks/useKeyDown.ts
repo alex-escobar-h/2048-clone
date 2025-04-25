c8f1b3df2d5839c187c8f2aa81f566b08d406d82
alex-escobar-h <escobar.alex5550@gmail.com>
2025-04-25 12:59:01 -0700

    feat: hook to handle keydown inputs

---
+++ src/hooks/useKeyDown.ts
@@ -0,0 +1,42 @@
+import { Dispatch, useCallback, useEffect } from 'react';
+
+import { CleanupAction, MoveAction } from '../types';
+
+export const useKeyDown = (dispatch: Dispatch<MoveAction | CleanupAction>) => {
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+
+      const key = e.key.toLowerCase();
+
+      switch (key) {
+        case 'arrowup':
+        case 'w':
+          dispatch({ type: 'up' });
+          break;
+        case 'arrowdown':
+        case 's':
+          dispatch({ type: 'down' });
+          break;
+        case 'arrowleft':
+        case 'a':
+          dispatch({ type: 'left' });
+          break;
+        case 'arrowright':
+        case 'd':
+          dispatch({ type: 'right' });
+          break;
+        default:
+          return;
+      }
+
+      dispatch({ type: 'cleanup' });
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [handleKeyDown]);
+};
