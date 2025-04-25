2cae166e8493494734714df22387a0b0a7093312
alex-escobar-h <escobar.alex5550@gmail.com>
2025-04-25 15:59:32 -0700

    use score state to display players score

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
+import { GameContext } from '../contexts/GameContext';
 import styles from '../styles/Scoreboard.module.css';
 export const Scoreboard = () => {
+  const { score } = useContext(GameContext);
+
   return (
     <section className={styles.container}>
       <div className={styles.logo}>2048</div>
       <div className={styles.scoreContainer}>
         <h2 className={styles.scoreTitle}>SCORE</h2>
-        <span className={styles.score}>0</span>
+        <span className={styles.score}>{score}</span>
       </div>
     </section>
   );
